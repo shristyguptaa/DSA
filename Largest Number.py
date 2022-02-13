3b0d8ffa31d99809667f7ccaab7376138dd37192
Shristy Gupta <89759891+shristyguptaa@users.noreply.github.com>
2022-02-13 13:31:09 +0530

    Create Largest Number.py

---
+++ Largest Number.py
@@ -0,0 +1,15 @@
+# Largest Number
+nums = [10,2]
+
+def greaterOrEqual(self, digit, max_digit):
+    print (int(str(digit) + str(max_digit)) >= int(str(max_digit) + str(digit)))
+
+ans = ''
+while nums != []:
+    maxDigit = 0
+    for num in nums:
+        if self.greaterOrEqual(num, maxDigit):
+            maxDigit = num
+    ans += str(maxDigit)
+    nums.remove(maxDigit)
+print (0) if ans[0]=='0' else ans
