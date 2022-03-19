12b95ea6e89e69b48110993b023921ba6e9d3198
Shristy Gupta <89759891+shristyguptaa@users.noreply.github.com>
2022-03-19 15:12:53 +0530

    Update Longest Increasing Subsequence.py

--- Longest Increasing Subsequence.py
+++ Longest Increasing Subsequence.py
@@ -1,13 +1,10 @@
 # Longest Increasing Subsequence
 array = [10, 9, 2, 5, 3, 7, 101, 18]
 maxSeq = [1 for a in array]
-seq = [None for x in array]
 for i in range(len(array)):
     currNum = array[i]
     for j in range(i):
         otherNum = array[j]
-        if currNum>otherNum and maxSeq[i]< maxSeq[j]+1:
-            maxSeq[i] = maxSeq[j]+1
-            seq[i] = j
-print(max(maxSeq))
-print(seq)     
+        if currNum>otherNum:
+            maxSeq[i] = max(maxSeq[i], maxSeq[j]+1)
+return (max(maxSeq))
