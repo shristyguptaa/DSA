6812aee9d674be5316e873831ef92902e3118e50
Shristy Gupta <89759891+shristyguptaa@users.noreply.github.com>
2022-03-30 10:22:24 +0530

    Update Set Matrix Zeroes.py

--- Set Matrix Zeroes.py
+++ Set Matrix Zeroes.py
@@ -28,3 +28,19 @@ for j in range(len(col)):
     if col[j] == 0:
         for i in range(len(matrix)):
             matrix[i][j] = 0
+
+# Solution 3
+rows = set()
+cols = set()
+for i in range(len(A)):
+    for j in range(len(A[0])):
+        if A[i][j]==0:
+            rows.add(i)
+            cols.add(j)
+for i in rows:
+    for j in range(len(A[0])):
+        A[i][j] = 0
+for j in cols:
+    for i in range(len(A)):
+        A[i][j] = 0
+return A
