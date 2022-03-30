cce351e1159114f616c4cd09d32fa46cb161c7bf
Shristy Gupta <89759891+shristyguptaa@users.noreply.github.com>
2022-03-30 10:36:00 +0530

    Create Search in Rotated Sorted Array.py

---
+++ Search in Rotated Sorted Array.py
@@ -0,0 +1,24 @@
+# Search in Rotated Sorted Array
+nums = [4,5,6,7,0,1,2]
+target = 0
+
+l, r = 0, len(nums)-1
+while l <=r:
+    mid = (l+r)//2
+    if target == nums[mid]:
+        return mid
+
+    # left sorted portion
+    if nums[l] <=nums[mid]:
+        if target > nums[mid] or target < nums[l]:
+            l = mid +1
+        else:
+            r = mid -1
+
+    # right sorted portion
+    else:
+        if target < nums[mid] or target > nums[r]:
+            r = mid -1
+        else:
+            l = mid +1
+return -1
