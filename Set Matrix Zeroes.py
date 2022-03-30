d6f4718832eb56162c3a19f205f51266854c1902
Shristy Gupta <89759891+shristyguptaa@users.noreply.github.com>
2022-03-30 10:19:56 +0530

    Update Set Matrix Zeroes.py

--- Set Matrix Zeroes.py
+++ Set Matrix Zeroes.py
@@ -1,6 +1,7 @@
 # Set Matrix Zeroes
 matrix = [[1,1,1],[1,0,1],[1,1,1]]
 
+# Solution 1
 tmp = [[matrix[i][j] for j in range(len(matrix[0]))] for i in range(len(matrix))]
 for i in range(len(matrix)):
     for j in range(len(matrix[0])):
@@ -10,3 +11,20 @@ for i in range(len(matrix)):
             for k in range(len(matrix)):
                 tmp[k][j] = 0
 matrix[:] = tmp[:]
+
+# Solution 2
+row = [1]*len(matrix)
+col = [1]*len(matrix[0])
+for i in range(len(matrix)):
+    for j in range(len(matrix[0])):
+        if matrix[i][j] == 0:
+            row[i] = 0
+            col[j] = 0
+for r in range(len(row)):
+    if row[r] == 0:
+        for i in range(len(matrix[0])):
+            matrix[r][i] = 0
+for j in range(len(col)):
+    if col[j] == 0:
+        for i in range(len(matrix)):
+            matrix[i][j] = 0
