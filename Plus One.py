ec4c670046e2c457de22bb2c4855f69d862b7e01
Shristy Gupta <89759891+shristyguptaa@users.noreply.github.com>
2022-04-21 06:39:49 -0700

    Create Plus One.py

---
+++ Plus One.py
@@ -0,0 +1,30 @@
+# Plus One
+digits = [1,2,3]
+
+# Solution 1
+num = 0
+for n in digits:
+    num = num*10+n
+num+=1
+return list (map(int,str(num)))
+
+# Solution 2
+carry = 0
+num = digits[-1] + 1
+if num <= 9:
+    digits[-1] = digits[-1]+1
+    return digits
+carry = 1
+digits[-1] = 0
+i = len(digits)-2
+while i>=0:
+    if digits[i] == 9 and carry == 1:
+        digits[i] = 0
+        carry = 1
+    elif carry == 1:
+        digits[i]+=1
+        carry = 0
+    i-=1
+if carry == 1 and i == -1:
+    digits.insert(0,1)
+return digits
